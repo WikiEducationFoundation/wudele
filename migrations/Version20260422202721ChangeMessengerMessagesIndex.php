9882ae75769e3996c28f3fa99934368e3203da8b
Marien Fressinaud <marien+dev@fressinaud.fr>
2026-04-22 22:28:43 +0200

    chore: Migrate Messenger messages index

---
+++ migrations/Version20260422202721ChangeMessengerMessagesIndex.php
@@ -0,0 +1,42 @@
+<?php
+
+// This file is part of Pollaris.
+// Copyright 2024-2026 Marien Fressinaud
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+// phpcs:disable Generic.Files.LineLength
+final class Version20260422202721ChangeMessengerMessagesIndex extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Change the Messenger messages index';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('DROP INDEX idx_75ea56e016ba31db');
+        $this->addSql('DROP INDEX idx_75ea56e0e3bd61ce');
+        $this->addSql('DROP INDEX idx_75ea56e0fb7336f0');
+        $this->addSql(<<<'SQL'
+            CREATE INDEX IDX_75EA56E0FB7336F0E3BD61CE16BA31DBBF396750 ON messenger_messages (
+              queue_name, available_at, delivered_at,
+              id
+            )
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP INDEX IDX_75EA56E0FB7336F0E3BD61CE16BA31DBBF396750');
+        $this->addSql('CREATE INDEX idx_75ea56e016ba31db ON messenger_messages (delivered_at)');
+        $this->addSql('CREATE INDEX idx_75ea56e0e3bd61ce ON messenger_messages (available_at)');
+        $this->addSql('CREATE INDEX idx_75ea56e0fb7336f0 ON messenger_messages (queue_name)');
+    }
+}
